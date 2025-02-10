c5fceb995aadeeb714e3a24cf484aa7148fdc5d5
RTMoo <toktarkazyramazan@gmail.com>
2025-02-11 00:36:07 +0500

    feat: Task model

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(to="accounts.CustomUser", on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=512, blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)

3423ba2dcb729ea00a1d5d4c6e98d49a3069a4ee
RTMoo <toktarkazyramazan@gmail.com>
2025-02-12 22:48:49 +0500

    fix: urls app tasks

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+
 from tasks import views
 
 urlpatterns = [
     path("", view=views.TaskAPIView.as_view(), name="list-create_task"),
     path(
-        "task/<int:pk>/",
+        "<int:pk>/",
         view=views.TaskAPIView.as_view(),
         name="get-update-delete_task",
     ),

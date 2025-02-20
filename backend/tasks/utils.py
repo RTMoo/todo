98b32ad6af74fa6d4a30d2d2c26352fe39d78bab
RTMoo <toktarkazyramazan@gmail.com>
2025-02-21 00:12:52 +0500

    [fix-back] Рефакторинг утилиты для фильтрации и сортировки данных

--- backend/tasks/utils.py
+++ backend/tasks/utils.py
@@ -3,25 +3,17 @@ from tasks.models import Task
 
 def filtration_data(request, query_dict):
     user = request.user.id
-    is_completed = query_dict.get("is_completed", None)
-    created_at = query_dict.get("created_at", None)
-
-    is_completed = None if is_completed is None else is_completed.lower() == "true"
-    created_at = (
-        None
-        if created_at is None
-        else "-created_at"
-        if created_at.casefold() == "desc"
-        else "created_at"
-    )
+    is_completed = query_dict.get("is_completed", "null").lower()
+    created_at = query_dict.get("created_at", "null").lower()
 
     filters = {"user": user}
-    if is_completed is not None:
-        filters["is_completed"] = is_completed
+    if is_completed in ("true", "false"):
+        filters["is_completed"] = is_completed == "true"
 
-    if created_at:
-        data = Task.objects.filter(**filters).order_by(created_at)
+    if created_at in ("asc", "desc"):
+        ordering = "created_at" if created_at == "asc" else "-created_at"
+        data = Task.objects.filter(**filters).order_by(ordering)
     else:
         data = Task.objects.filter(**filters)
 
-    return data if data else False
+    return data
